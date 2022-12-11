accd5387b19501b796c856c0fc121f99f667a488
Davin Thomas <dethomas1@gmail.com>
2022-12-11 11:08:47 -0600

    Built file structure

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// This is a basic express server.
+
+require("dotenv").config();               /* Gives access to the value of
+                                             PORT variable */
+const express = require("express");
+
+const app = express();                    /* Initialize express function in app 
+                                             variable.*/
+                                             
+
+const PORT = process.env.PORT || 5000;    /* Create port variable. */
+
+app.listen(PORT, () => console.log
+(`Server running on port ${PORT}`));      /* Allows to listen on port variable
+                                             that will be obtained from
+                                             environment variables and displays
+                                             confirmation message w/ port number. 
+                                             */
